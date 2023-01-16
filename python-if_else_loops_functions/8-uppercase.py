9af3f8365045eabf075629bcf571fc7d55e1254e
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2023-01-16 13:01:43 +0100

    Adding .format in print

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python3
 def uppercase(str):
     for c in range (len(str)):
         if ord(str[c]) >= 97 and ord(str[c]) <= 122:
-            print(chr(ord(str[c]) - 32), end = '')
+            print("{}".format(chr(ord(str[c]) - 32)), end = '')
         else:
-            print(str[c], end = '')
+            print("{}".format(str[c]), end = '')

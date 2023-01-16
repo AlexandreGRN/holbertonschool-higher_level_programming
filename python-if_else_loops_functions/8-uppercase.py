dac636928407ead190d86c4f71d51f8cd2b31997
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2023-01-16 13:28:40 +0100

    Removing most spaces

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for c in range (len(str)):
+    for c in range(len(str)):
         u = ord(str[c])
         if u >= 97 and u <= 122:
             u = u - 32
-        print("{}".format(chr(u)), end = '')
+        print("{}".format(chr(u)), end='')
     print("")

24e730bd28eac7dd731e064baa00497d1473cc0e
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2023-01-16 13:51:32 +0100

    Create 12-fizzbuzz.py

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+def fizzbuzz():
+    for i in range(1, 101):
+        # i multiples of 3 & 5
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=' ')
+        # i only multiples of 3
+        elif i % 3 == 0 and i % 5 != 0:
+            print("Fizz", end=' ')
+        # i only multiples of 5
+        elif i % 3 != 0 and i % 5 == 0:
+            print("Buzz", end=' ')
+        # i isn't neither a multiples of 3 nor 5
+        else:
+            print("{}".format(i), end=' ')

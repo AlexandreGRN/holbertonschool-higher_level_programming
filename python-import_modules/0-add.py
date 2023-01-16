3e63e512fc4bcc459972eed7e45c02ad56d3f8a9
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2023-01-16 15:09:14 +0100

    if main = main for some reason

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from python-import_modules.packages.add_0.py import add
 
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+if __name__ == "__main__":
+    from python-import_modules.packages.add_0.py import add
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))

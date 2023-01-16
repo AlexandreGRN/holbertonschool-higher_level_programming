e0674a0da4746d4307e4bf3f5db119aec1abe794
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2023-01-16 15:10:59 +0100

    Removing one iteration of the name file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     from python-import_modules.packages.add_0.py import add
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))

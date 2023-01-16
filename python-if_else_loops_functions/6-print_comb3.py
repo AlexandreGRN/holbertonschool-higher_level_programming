d01fd11b234a1a268527ee48e9094333124d2500
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2023-01-16 12:32:21 +0100

    Create 6-print_comb3.py

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for j in range(9):
+    i = j + 1
+    for i in range(i, 10):
+        if j != 0 | i != 1:
+            print(', ', end='')
+        print("{}{}".format(j, i), end='')
+    i = i + 1

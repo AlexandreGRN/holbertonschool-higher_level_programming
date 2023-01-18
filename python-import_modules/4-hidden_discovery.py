6fe46782bb1d142cf41d5b4a8f8f8b2414fb87c0
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2023-01-18 20:39:13 +0000

    PEP8 v2

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
 if __name__ == "__main__":
-    # go through the whole file // dir return an array of strings -> i == string
+    # go through the whole file // dir return an array of strings -> i = string
     for i in (dir(hidden_4)):
         # search for target words then print it
         if i[0:2] != "__":

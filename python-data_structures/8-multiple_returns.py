915369292a9af5b7cb6f11f0f966a6332ee62825
AlexandreGRN <tulkiidra@gmail.com>
2023-01-23 05:08:17 -0800

    Remove main.c

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -3,7 +3,3 @@ def multiple_returns(sentence):
     if not sentence or sentence == "":
         return (0, None)
     return (len(sentence), sentence[0])
-
-sentence = ""
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))

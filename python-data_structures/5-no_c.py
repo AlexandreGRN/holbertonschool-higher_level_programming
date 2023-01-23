81bb78072fa992a0ed7db02d0a1b3773e7c1bb34
AlexandreGRN <tulkiidra@gmail.com>
2023-01-23 04:46:06 -0800

    Push every tasks

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_list = []
+    new_str = ""
+    for i in range(len(my_string)):
+        if my_string[i] != 'c' and my_string[i] != 'C':
+            new_list.append(my_string[i])
+    new_str = ''.join(new_list)
+    return new_str
+
+print(no_c("Best School"))
+print(no_c("Chicago"))
+print(no_c("C is fun!"))

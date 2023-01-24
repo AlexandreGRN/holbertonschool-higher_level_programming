1ffde7688b520acd59e8f8aec93e121d0c898c28
Alexandre Guerin <113639808+AlexandreGRN@users.noreply.github.com>
2023-01-24 14:10:55 +0100

    Update 12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -7,7 +7,7 @@ def roman_to_int(roman_string):
                  'C': 100, 'D': 500,
                  'M': 1000}
     # test if string or not & null string
-    if roman_string == "" :
+    if not isinstance(roman_string, str) or roman_string == "":
         return 0
 
     # convert string into list of int

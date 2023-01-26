2b7a8c29f38acd098b250de1b1f5cc7736806347
AlexandreGRN <tulkiidra@gmail.com>
2023-01-26 08:29:37 -0800

    Most PEP8

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-def safe_print_list_integers(my_list=[],x=0):
+def safe_print_list_integers(my_list=[], x=0):
     numberPrint = 0
     for i in range(x):
         my_list[i]
         try:
-            print("{:d}".format(my_list[i]), end = "")
+            print("{:d}".format(my_list[i]), end="")
             numberPrint += 1
         except:
             numberPrint = numberPrint
     print()
     return numberPrint
-
--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-def safe_print_division(a,b):
+def safe_print_division(a, b):
     try:
         print("Inside result:", a/b)
         return a/b
     except:
         print("Inside result: None")
         return None
+    finally:
+        a

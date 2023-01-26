2e2c27d92a0be6f60422803a30f208482b5fe460
AlexandreGRN <tulkiidra@gmail.com>
2023-01-26 09:08:16 -0800

    Test to reassign tuples before using them

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    num1 = 0
-    num2 = 0
-    if len(tuple_a[0]) > 0:
-        num1 = num1 + tuple_a[0]
-        if len(tuple_a[1]) > 0:
-            num2 = num2 + tuple_a[1]
-    if len(tuple_b[0]) > 0:
-        num1 = num1 + tuple_b[0]
-        if len(tuple_b[1]) > 0:
-            num2 = num2 + tuple_a[1]  
-    return (num1, num2)
+    tuple_a = tuple_a + (0, 0)
+    tuple_b = tuple_b + (0, 0)
+    
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])

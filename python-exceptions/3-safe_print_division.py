401d9bc9e6dbb41ac40a9f10685e86a29c7050a1
AlexandreGRN <tulkiidra@gmail.com>
2023-01-26 08:32:19 -0800

    Adding finally at the end

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        print("Inside result:", a/b)
+        print("Inside result: {}".format(a/b))
         return a/b
     except:
         print("Inside result: None")

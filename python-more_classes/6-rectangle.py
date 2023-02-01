f1a5b7d3f4b1c0bad0fb160423537b370d7ea95d
AlexandreGRN <tulkiidra@gmail.com>
2023-02-01 10:20:37 -0800

    not working task 6

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -1,1 +1,84 @@
 #!/usr/bin/python3
+""" This program define a rectangle """
+
+
+class Rectangle:
+    """ class that will define the rectangle precisely """
+    number_of_instances = 0
+    def __init__(self, width=0, height=0):
+        self.number_of_instances += 1
+        print(self.number_of_instances)
+        self.width = width
+        self.height = height
+
+    # ----------------- Width
+    @property
+    def width(self):
+        """ define the width value """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ set the value of width """
+        if value is None:
+            raise TypeError("width must be an integer")
+        if not isinstance(value, (int, float)):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    # ----------------- Height
+    @property
+    def height(self):
+        """ define the height value """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ set the value of height """
+        if value is None:
+            raise TypeError("height must be an integer")
+        if not isinstance(value, (int, float)):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    # ----------------- Area and perimeter calc
+
+    def area(self):
+        """ calculate the area of the rectangle """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """ calculate the perimeter of the rectangle """
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return self.__height * 2 + self.__width * 2
+
+    # ----------------- Create the thing to print
+
+    def __str__(self):
+        string = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                string = string + '#'
+            if i != self.__height - 1 and self.__width != 0:
+                string = string + '\n'
+        return(string)
+
+    def __repr__(self):
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        print("Bye rectangle...")
+        self.number_of_instances -= 1
+
+my_rectangle_1 = Rectangle(2, 4)
+my_rectangle_2 = Rectangle(2, 4)
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+del my_rectangle_1
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+del my_rectangle_2
+print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))

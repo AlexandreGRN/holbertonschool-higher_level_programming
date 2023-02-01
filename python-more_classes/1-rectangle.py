4998c379dcbbd585829814b3b6f1d53906d2890d
AlexandreGRN <tulkiidra@gmail.com>
2023-02-01 02:40:04 -0800

    PEP8 + deprivating things

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,10 +5,10 @@
 class Rectangle:
     """ class that will define the rectangle precisely """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
         
-    ############### Width
+    # ----------------- Width
     @property
     def width(self):
         """ define the width value """
@@ -17,7 +17,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """ set the value of width """
-        if value == None:
+        if value is None:
             raise TypeError("width must be an integer")
         if not isinstance(value, (int, float)):
             raise TypeError("width must be an integer")
@@ -25,16 +25,16 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-    ############### Height
+    # ----------------- Height
     @property
     def height(self):
         """ define the height value """
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """ set the value of height """
-        if value == None:
+        if value is None:
             raise TypeError("height must be an integer")
         if not isinstance(value, (int, float)):
             raise TypeError("height must be an integer")

3b20a174d1642cc33de55431522dcd54ef9d5607
AlexandreGRN <tulkiidra@gmail.com>
2023-02-01 05:12:04 -0800

    Upload task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,18 +1,66 @@
 #!/usr/bin/python3
-""" This module is a first creation of empty class for a square """
+""" This program define a rectangle """
 
 
-class Square:
+class Rectangle:
+    """ class that will define the rectangle precisely """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
 
-    """Empty class"""
-    def __init__(self, size=0):
-        """initialisation of the size, with some constraint"""
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+    # ----------------- Width
+    @property
+    def width(self):
+        """ define the width value """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ set the value of width """
+        if value is None:
+            raise TypeError("width must be an integer")
+        if not isinstance(value, (int, float)):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    # ----------------- Height
+    @property
+    def height(self):
+        """ define the height value """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ set the value of height """
+        if value is None:
+            raise TypeError("height must be an integer")
+        if not isinstance(value, (int, float)):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    # ----------------- Area and perimeter calc
 
     def area(self):
-        """return the square area"""
-        return (self.__size)**2
+        """ calculate the area of the rectangle """
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """ calculate the perimeter of the rectangle """
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return self.__height * 2 + self.__width * 2
+
+    # ----------------- Print rectangle
+
+my_rectangle = Rectangle(2, 4)
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
+
+print("--")
+
+my_rectangle.width = 10
+my_rectangle.height = 3
+print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))

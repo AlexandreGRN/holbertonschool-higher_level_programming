089e57a4eb39ab5a7532c60b36c86fde3e7445a0
AlexandreGRN <tulkiidra@gmail.com>
2023-02-02 02:30:53 -0800

    blank line

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     """ class that will define the rectangle precisely """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         Rectangle.number_of_instances += 1
         self.width = width

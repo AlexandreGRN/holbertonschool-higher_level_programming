4c5c36d64612859478cc1572c49bb0d989989f2a
AlexandreGRN <tulkiidra@gmail.com>
2023-02-02 09:48:15 -0800

    Seems working fine

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Indent and print a text line by line """
+
+
+def text_indentation(text):
+    """ Micka == bad """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    text = text.replace("\n", "")
+    text = text.replace(".", "\n\n")
+    text = text.replace("?", "\n\n")
+    text = text.replace(":", "\n\n")
+    
+    print(text, end="")

3b6ab4069ebf9f7647f023b275c0c09c0ab6b026
AlexandreGRN <tulkiidra@gmail.com>
2023-02-02 03:40:04 -0800

    Try exepct

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+""" Hello """
+
 
 def add_integer(a, b=98):
     """ Function that add 2 ints"""
-    if not isinstance(a, (int, float)):
+    try:
+        num1 = int(a)
+    except:
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    try:
+        num2 = int(b)
+    except:
         raise TypeError("b must be an integer")
     return (int(a)+int(b))

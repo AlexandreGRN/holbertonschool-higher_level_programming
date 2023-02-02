0343eebc0d71f9c86f3e7bf855e654dc795f585c
AlexandreGRN <tulkiidra@gmail.com>
2023-02-02 03:41:37 -0800

    Horrible project

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,11 +5,11 @@
 def add_integer(a, b=98):
     """ Function that add 2 ints"""
     try:
-        num1 = int(a)
+        a = int(a)
     except:
         raise TypeError("a must be an integer")
     try:
-        num2 = int(b)
+        b = int(b)
     except:
         raise TypeError("b must be an integer")
-    return (int(a)+int(b))
+    return (a+b)

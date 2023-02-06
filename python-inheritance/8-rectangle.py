af732aac88635e7f82ad652910ae5e065dbcc6e6
AlexandreGRN <tulkiidra@gmail.com>
2023-02-06 07:31:26 -0800

    Removing one part of my code

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,21 +2,6 @@
 """ check for good value value """
 
 
-class BaseGeometry:
-    """ class BaseGeometry """
-
-    def area(self):
-        """ Nothing useless for now """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Validate value type """
-        if not type(value) == int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
 class Rectangle(BaseGeometry):
     """ class: rectangle (parent: BaseGeometry) """
 

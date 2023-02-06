1cb58de46bf478735053eb10168dde9dd3a38cc1
AlexandreGRN <tulkiidra@gmail.com>
2023-02-06 07:31:54 -0800

    Import

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ check for good value value """
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """ class: rectangle (parent: BaseGeometry) """

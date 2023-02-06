67cbf0f4fab2956d288f9e166d0b995f86e9b5e0
AlexandreGRN <tulkiidra@gmail.com>
2023-02-06 07:21:13 -0800

    Upload task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,2 +1,23 @@
 #!/usr/bin/python3
-def inherits_from(obj,a_class):
+""" check for good value value """
+
+
+class BaseGeometry:
+    """ base geometry class"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ Nothing useless for now """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Validate value type """
+        if not type(value) == int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))

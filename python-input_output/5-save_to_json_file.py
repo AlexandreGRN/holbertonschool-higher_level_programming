ff0ba9553c1c8579ce595ce44722fa88c800b686
AlexandreGRN <tulkiidra@gmail.com>
2023-02-07 07:50:37 -0800

    Upload task 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """ doc """
+import json
 
 
 def save_to_json_file(my_obj,filename):
-    
+    """ write the json representation of my_obj """
+    with open(filename, "w", encoding="utf-8") as f:
+         f.write(json.dumps(my_obj))
+         f.close

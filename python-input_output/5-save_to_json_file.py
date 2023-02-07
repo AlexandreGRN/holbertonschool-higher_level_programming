830afc91dd1ac5feadfa300e6918fe3efce462bf
AlexandreGRN <tulkiidra@gmail.com>
2023-02-07 07:52:48 -0800

    PEP8

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -3,8 +3,8 @@
 import json
 
 
-def save_to_json_file(my_obj,filename):
+def save_to_json_file(my_obj, filename):
     """ write the json representation of my_obj """
     with open(filename, "w", encoding="utf-8") as f:
-         f.write(json.dumps(my_obj))
-         f.close
+        f.write(json.dumps(my_obj))
+        f.close

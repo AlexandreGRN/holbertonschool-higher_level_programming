bf0a953987d16ef1736d4f89f9b678284bad7cc3
AlexandreGRN <tulkiidra@gmail.com>
2023-02-07 10:28:51 -0800

    Task 12 print hello perfectly

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
-def class_to_json(obj):
+def pascal_triangle(n):
+    """ create pascal trueiangle"""
+    pascal = [[1]]
+    for rowNumber in range(1, n):
+        """ for each new row of numbers """
+        row = []
+        for numbersNumber in range(rowNumber + 1):
+            row.append("hello")
+        pascal.append(row)
+    return pascal
+        
+def print_triangle(triangle):
+    """
+    Print the triangle
+    """
+    for row in triangle:
+        print("[{}]".format(",".join([str(x) for x in row])))
+
+
+if __name__ == "__main__":
+    print_triangle(pascal_triangle(4))

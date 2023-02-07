f10b5fbfa033e0eafd465f468f8c01264a183c50
AlexandreGRN <tulkiidra@gmail.com>
2023-02-07 06:55:30 -0800

    Upload task 3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+""" doc """
+import json
+
+
 def to_json_string(my_obj):
+    """ transform an object(type: dictionnary) into a json rprent of obj """
+    return json.dumps(my_obj)

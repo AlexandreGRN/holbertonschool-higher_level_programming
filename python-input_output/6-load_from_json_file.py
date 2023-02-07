ef025054650258774951288f5e11db04e93bbbd2
AlexandreGRN <tulkiidra@gmail.com>
2023-02-07 08:53:37 -0800

    Upload task 6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+""" doc """
+import json
+
+
 def load_from_json_file(filename):
+    """ get the string from a file & loads a json from it """
+    with open(filename, "r", encoding="utf-8") as f:
+        strFile = f.read()
+        return json.loads(strFile)

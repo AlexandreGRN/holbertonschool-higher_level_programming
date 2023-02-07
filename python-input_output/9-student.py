e14d0bafeddb2465f52f21eee3e33b78238d0bee
AlexandreGRN <tulkiidra@gmail.com>
2023-02-07 12:05:38 -0800

    Upload task 9

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-def class_to_json(obj):
+""" doc """
+class Student:
+    """ Student class """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initialisation """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ retrieve a dictionary representation of a student var """
+        return self.__dict__

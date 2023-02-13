fba61379183033391a67fba83376abc82c575df6
AlexandreGRN <tulkiidra@gmail.com>
2023-02-13 07:54:49 -0800

    PEP8 changing

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,12 +6,12 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     """ Square class """
-    
+
     def __init__(self, size, x=0, y=0, id=None):
         """ initialisation """
         super().__init__(size, size, x, y, id)
         self.size = size
-    
+
     @property
     def size(self):
         """ size getter """

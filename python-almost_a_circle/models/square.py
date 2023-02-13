3ba143cca477f92d60c50c06958051cd0e9d7a0b
AlexandreGRN <tulkiidra@gmail.com>
2023-02-13 09:24:42 -0800

    Adding setter decorator

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,23 +9,24 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """ initialisation """
-        super().__init__(size, size, x, y, id)
-        self.size = size
+        super().__init__(height=size, width=size, x=x, y=y, id=id)
 
     @property
     def size(self):
         """ size getter """
-        return self.__size
+        return self.width
 
+    @size.setter
     def size(self, value):
         """ size setter """
         if value is None or type(value) != int:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
-        self.__size = value
+        self.height = value
+        self.width = value
 
     def __str__(self):
         """ print the square infos """
         str = "[Square] ({}) {}/{} - {}"
-        return str.format(self.id, self.x, self.y, self.size)
+        return str.format(self.id, self.x, self.y, self.height)

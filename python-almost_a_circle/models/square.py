af182cdb4fe7e64b43ab7c38dfe54302b21db21e
AlexandreGRN <tulkiidra@gmail.com>
2023-02-13 07:54:09 -0800

    Adding id handling for square class

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,7 +9,7 @@ class Square(Rectangle):
     
     def __init__(self, size, x=0, y=0, id=None):
         """ initialisation """
-        super().__init__(size, size, x, y)
+        super().__init__(size, size, x, y, id)
         self.size = size
     
     @property

c7c2f044c51e6ee6204292bb6b800943159c7375
AlexandreGRN <tulkiidra@gmail.com>
2023-02-13 06:13:20 -0800

    Adding tests for initialization

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,15 +1,34 @@
 #!/usr/bin/python3
 """ Creating rectangle Class """
-from models.base import Base
+from base import Base
 
 
 class Rectangle(Base):
     """ Rectangle Class """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        if width is None or type(width) != int:
+            raise TypeError("width must be an integer")
+        if width <= 0:
+            raise ValueError("width must be > 0")
         self.__width = width
+
+        if height is None or type(height) != int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
+
+        if x is None or type(x) != int:
+            raise TypeError("x must be an integer")
+        if x <= 0:
+            raise ValueError("x must be > 0")
         self.__x = x
+
+        if y is None or type(y) != int:
+            raise TypeError("y must be an integer")
+        if y <= 0:
+            raise ValueError("y must be > 0")
         self.__y = y
         super().__init__(id)
 

bbbf1a5a1e1831d59519438de0a0c05bdd806c14
AlexandreGRN <tulkiidra@gmail.com>
2023-02-13 05:34:10 -0800

    Removing more whitespaces

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,14 +5,14 @@ from models.base import Base
 
 class Rectangle(Base):
     """ Rectangle Class """
-    
+
     def __init__(self, width, height, x=0, y=0, id=None):
         self.__width = width
         self.__height = height
         self.__x = x
         self.__y = y
         super().__init__(id)
-    
+
     # ---------- Width
     @property
     def width(self):
@@ -52,7 +52,7 @@ class Rectangle(Base):
         """ get the height value """
         return self.__y
 
-    @width.setter
+    @y.setter
     def y(self, value):
         """ set the height value to @height """
         self.__y = value

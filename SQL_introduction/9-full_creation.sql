917c97a902a2f7d601464ac16adabd4215bdf550
AlexandreGRN <tulkiidra@gmail.com>
2023-03-13 10:39:42 -0700

    Adding ";" because i need to first create the tabl

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Create a full database with already existing infos
-CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT)
+CREATE TABLE IF NOT EXISTS second_table(id int, name VARCHAR(256), score int);
 INSERT INTO second_table(
     id,
     name,

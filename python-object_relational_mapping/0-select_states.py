9f7bc6c09f773f720a85e0bdc06da52a95828639
AlexandreGRN <tulkiidra@gmail.com>
2023-03-14 14:18:08 -0700

    PEP8

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,8 +9,8 @@ if len(sys.argv) == 4:
     password_ = sys.argv[2]
     database_ = sys.argv[3]
 
-    db=MySQLdb.connect(host = "localhost", user = username_,
-                       password = password_, database = database_, port = 3306)
+    db = MySQLdb.connect(host="localhost", user=username_,
+                       password=password_, database=database_, port=3306)
 
     # Action
     cursor = db.cursor()

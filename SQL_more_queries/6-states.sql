66954b118fb7c9bbbfd4316f90acc9b50b2ddd9d
AlexandreGRN <tulkiidra@gmail.com>
2023-03-14 06:23:40 -0700

    with autoincrement

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -3,8 +3,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states
     (
-        id INT                UNIQUE NOT NULL AUTO_INCREMENT
-        name VARCHAR(256)     NOT NULL
+        id INT                UNIQUE NOT NULL AUTO_INCREMENT,
+        name VARCHAR(256)     NOT NULL,
 
-        PRIMARY KEY (id)
-    )
+        PRIMARY KEY (ID)
+    );

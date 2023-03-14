9b111489c80b61a9c43a95146b18a66406a98cf9
AlexandreGRN <tulkiidra@gmail.com>
2023-03-14 15:06:07 -0700

    List all cities

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" doc """
+import MySQLdb
+import sys
+
+if len(sys.argv) >= 4:
+    # Connection
+    username_ = sys.argv[1]
+    password_ = sys.argv[2]
+    database_ = sys.argv[3]
+
+    db = MySQLdb.connect(host="localhost", user=username_,
+                         password=password_, database=database_, port=3306)
+
+    # Action
+    cursor = db.cursor()
+
+    # SQL Command
+    querry = """
+    SELECT *
+        FROM cities
+        ORDER BY cities.id ASC
+    """
+
+    cursor.execute(querry)
+    # Print
+    for i in cursor.fetchall():
+        print(i)
+
+    db.close()

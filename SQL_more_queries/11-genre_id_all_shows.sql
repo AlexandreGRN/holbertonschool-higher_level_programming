944e0eda30a4ca40c88871dd62e035c4af011031
AlexandreGRN <tulkiidra@gmail.com>
2023-03-15 06:05:54 -0700

    LEFT join

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,11 +1,6 @@
 -- genre id all shows
 SELECT tv_shows.title, tv_show_genres.genre_id
-    FROM tv_shows, tv_show_genres
-    WHERE
-        CASE
-            WHEN tv_show_genres.genre_id NOT NULL
-                THEN tv_shows.id = tv_show_genres.genre_id
-                ELSE tv_show_genres.genre_id = 'NULL'
-        END
-
+    FROM tv_shows
+    LEFT JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
     ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;

a47a5d62728fbd4bdb42a37a03536db46ba077e7
AlexandreGRN <tulkiidra@gmail.com>
2023-04-03 15:28:40 -0700

    w

--- SQL_more_queries/hbtn_0d_tvshows.sql
+++ SQL_more_queries/hbtn_0d_tvshows.sql
@@ -89,7 +89,7 @@ LOCK TABLES `tv_shows` WRITE;
 INSERT INTO `tv_shows` VALUES (1,'House'),(2,'Game of Thrones'),(3,'The Big Bang Theory'),(4,'New Girl'),(5,'Silicon Valley'),(6,'Breaking Bad'),(7,'Better Call Saul'),(8,'Dexter'),(9,'Homeland'),(10,'The Last Man on Earth');
 /*!40000 ALTER TABLE `tv_shows` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE *
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
